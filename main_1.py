1f25763ca81f853e5c3e31b97186dab6f025f370
Mauro <skovi@gmail.com>
2024-11-06 11:40:06 -0300

    aula5

---
+++ main_1.py
@@ -0,0 +1,31 @@
+# comentario
+# --
+# print ("hello wolrd")
+# print ('segundo hello wolrd')
+# print ("terceiro hello wolrd" + " teste1")
+# nome = input ("seu nome: ")
+# print (f"seje bem vindo:  {nome}")
+# -------------
+# numero_1: float(input("Digite 1: "))
+# numero_2: float(input("Digite 2: "))
+# soma: float(numero_1) + float(numero_2)
+# print(f"o resultado é: {soma}")
+#---------------------
+# x="0,1111"
+# print(x)
+# x = "carro"
+# print(x)
+#------------------
+print("seja bem vindo")
+nome = input("seu mome: ")
+salario = float(input("dig. salario: "))
+bonus = float(input("dig. bonus: "))
+bonus_total = 1000 + salario * bonus
+print(f"salario total: {bonus_total}")
+#
+print("Seja bem-vindo!")
+nome = input("Seu nome: ")
+salario = float(input("Digite o salário: ").replace(',', '.'))
+bonus = float(input("Digite o bônus: ").replace(',', '.'))
+bonus_total = 1000 + salario * bonus
+print(f"Salário total: {bonus_total}")
